d287fedcd50b791479e855f723798c8d0eebc244
rcjk <ryan9590@yahoo.com>
2024-01-12 10:28:02 -0800

    version 3

---
+++ app_data.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+maintenance = { "Select Service" : [],
+               "Brake Fluid": [6, 30000],
+               "Cabin Air Filter": [6, 30000],
+               "Cooling Flush": [6, 30000],
+               "Engine Air Filter": [6, 30000],
+               "Oil Change": [6, 30000],
+               "PCV": [6, 30000],
+               "Sparks Plugs": [6, 30000],
+               "Transmission Fluid": [6, 30000],
+               "Timing Belt": [6, 30000],
+               "Differential Oil": [6, 30000],
+               "Transfer Case Oil": [6, 30000],
+               "Starter": [6, 30000]}
+
+cars = ["Subaru Outback", "Honda Civic", "Toyota T100", "Toyota Tundra"]
